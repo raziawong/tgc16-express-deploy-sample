9db50b547c88207cc232c37a6e137323bc109741
Razia <nrwpc.stack@gmail.com>
2022-03-08 14:46:58 +0800

    update app.listen

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(express.json());
 // enable CORS 
 app.use(cors());
 
-const COLLECTION_NAME="food_sightings";
+const COLLECTION_NAME = "food_sightings";
 
 // Routes
-async function main(){
+async function main() {
     await MongoUtil.connect(process.env.MONGO_URI, "tgc16_food_sightings");
 
-    app.get('/welcome', function(req,res){
+    app.get('/welcome', function(req, res) {
         res.json({
-            'message':'Welcome to the free food sighting api!'
+            'message': 'Welcome to the free food sighting api!'
         })
     })
 
@@ -37,51 +37,51 @@ async function main(){
     //    put: replace existing info
     // 3. design the body
     //    what information does the user have to pass you?
-    app.post('/free_food_sighting', async function(req,res){
+    app.post('/free_food_sighting', async function(req, res) {
 
         try {
-        // to create a free food sighting:
-        // - description
-        // - comma separated string of the food
-        // - datetime
-        let description = req.body.description;
-        let food = req.body.food.split(',');
-        let datetime = new Date(req.body.datetime);
+            // to create a free food sighting:
+            // - description
+            // - comma separated string of the food
+            // - datetime
+            let description = req.body.description;
+            let food = req.body.food.split(',');
+            let datetime = new Date(req.body.datetime);
 
-        // insert into mongo database
-        const db = MongoUtil.getDB();
-        // db.collection('food_sightings').insertOne({
-        //     'description':description,
-        //     'food':food,
-        //     'date':datetime
-        // })
+            // insert into mongo database
+            const db = MongoUtil.getDB();
+            // db.collection('food_sightings').insertOne({
+            //     'description':description,
+            //     'food':food,
+            //     'date':datetime
+            // })
 
-        await db.collection(COLLECTION_NAME).insertOne({
-            description,
-            food,
-            datetime
-        });
-        res.status(200);
-        res.json({
-            'message':'The record has been added successfully'
-        })
-        } catch (e){
+            await db.collection(COLLECTION_NAME).insertOne({
+                description,
+                food,
+                datetime
+            });
+            res.status(200);
+            res.json({
+                'message': 'The record has been added successfully'
+            })
+        } catch (e) {
             res.status(500);
             res.json({
-                'message':"Internal server error. Please contact administrator"
+                'message': "Internal server error. Please contact administrator"
             })
             console.log(e);
         }
     })
 
-    app.get('/free_food_sighting', async function(req,res){
+    app.get('/free_food_sighting', async function(req, res) {
         // create citeria object (assumption: the user wants everything)
         let criteria = {};
 
         if (req.query.description) {
             criteria['description'] = {
                 '$regex': req.query.description,
-                '$options':'i'
+                '$options': 'i'
             }
         }
 
@@ -98,8 +98,8 @@ async function main(){
         })
     })
 
-    app.put('/free_food_sighting/:id', async function(req,res){
-        let {description, food, datetime} = req.body;
+    app.put('/free_food_sighting/:id', async function(req, res) {
+        let { description, food, datetime } = req.body;
         /*
             let description = req.body.description;
             let food = req.body.food;
@@ -108,32 +108,32 @@ async function main(){
         food = food.split(',');
 
         // remove all whitespaces from the front and back
-        food = food.map(function(each_food_item){
+        food = food.map(function(each_food_item) {
             return each_food_item.trim();
         })
         datetime = new Date(datetime);
         let results = await MongoUtil.getDB().collection(COLLECTION_NAME).updateOne({
             '_id': ObjectId(req.params.id)
-        },{
-            '$set':{
+        }, {
+            '$set': {
                 'description': description,
-                'food':food,
-                'datetime':datetime
+                'food': food,
+                'datetime': datetime
             }
         })
         res.status(200);
         res.json({
-            'message':'Food sighting has been updated'
+            'message': 'Food sighting has been updated'
         })
     })
 
-    app.delete('/free_food_sighting/:id', async function (req,res){
+    app.delete('/free_food_sighting/:id', async function(req, res) {
         await MongoUtil.getDB().collection(COLLECTION_NAME).deleteOne({
-            '_id':ObjectId(req.params.id)
+            '_id': ObjectId(req.params.id)
         })
         res.status(200);
         res.json({
-            'message':'The document has been deleted'
+            'message': 'The document has been deleted'
         })
     })
 }
@@ -141,6 +141,6 @@ async function main(){
 main();
 
 // Listen (must be the last)
-app.listen(3000, function(){
+app.listen(process.env.PORT, function() {
     console.log("Server has started")
 })
